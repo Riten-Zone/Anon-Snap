7fe048cf55a818ae7748a011ab0305d029cd1f91
Tradfibaby <tradfibaby@proton.me>
2026-01-18 23:21:57 +0000

    Suppress the warning when loading the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, LogBox} from 'react-native';
+
+LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered']);
 
 import {
   SplashScreen,
